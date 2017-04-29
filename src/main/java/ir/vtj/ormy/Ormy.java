a4e009ab1d4ea811c4584d79c71f87a3399ef40b
javadghane <13731373>
2017-04-29 14:51:48 +0430

    Create file and write is Ok

--- src/main/java/ir/vtj/ormy/Ormy.java
+++ src/main/java/ir/vtj/ormy/Ormy.java
@@ -8,7 +8,9 @@ import android.content.pm.PackageManager;
 import com.google.gson.Gson;
 
 import java.io.File;
-import java.lang.reflect.Field;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by JavaDroid on 4/26/2017 in MEEDC-GIS department.
@@ -16,29 +18,61 @@ import java.lang.reflect.Field;
  */
 
 public class Ormy {
-    public File mainFolder;
     public static Context context;
+    public static Application application;
+    public static String dbName;
 
-    public static void Init(Application app) {
-        context = app.getApplicationContext();
-        PackageManager m = app.getPackageManager();
-        String s = app.getPackageName();
-        PackageInfo p = null;
+    private static String getFileName() {
+        PackageManager m = application.getPackageManager();
+        String s = application.getPackageName();
+        PackageInfo p = new PackageInfo();
         try {
             p = m.getPackageInfo(s, 0);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-        s = p.applicationInfo.dataDir;
-        // Utile.toast("The Directory is: " + s);
+        return p.applicationInfo.dataDir + "/Ormy/" + dbName + ".ormy";
+    }
+
+    public static void Init(Application app, String databaseName) {
+        context = app.getApplicationContext();
+        application = app;
+        dbName = databaseName;
+        makeFile(getFileName());
+    }
+
+    private static void write(String text) {
+        try {
+            PrintWriter writer = new PrintWriter(getFileName(), "UTF-8");
+            writer.println(text);
+            writer.close();
+        } catch (IOException e) {
+            Utile.toast(e.getMessage());
+        }
+    }
+
+    private static void makeFile(String sFileName) {
+        try {
+            File root = new File(sFileName.substring(0, sFileName.indexOf("/")));
+            if (!root.exists()) {
+                root.mkdirs();
+            }
+            File gpxfile = new File(root, sFileName);
+            FileWriter writer = new FileWriter(gpxfile);
+            //writer.append(sBody);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void save(Object object) {
         Gson gson = new Gson();
         String a = gson.toJson(object);
-        Utile.toast(a);
+        write(a);
 
-        Class test = object.getClass();
+       /* Class test = object.getClass();
         Utile.toast("Fields" + test.getFields().length);
 
         for (Field field : test.getFields()) {
@@ -47,7 +81,7 @@ public class Ormy {
                 ir.vtj.ormy.Utile.toast(ormyTypeAnnotation.isPrimaryKey() ? "True" : "False");
             else
                 ir.vtj.ormy.Utile.toast("isNull");
-        }
+        }*/
     }
 
 
